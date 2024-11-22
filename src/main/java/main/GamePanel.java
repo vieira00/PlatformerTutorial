450a5e9f57d3f717c0fcbd46b32b160fa981e463
vieira00 <matheusricardov563@gmail.com>
2024-11-22 11:11:52 -0400

    Foi criada uma classe 'Game'; é aqui que somamos tudo.

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -1,4 +1,15 @@
 package main;
 
-public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    public GamePanel() {
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.fillRect(100,100, 200, 50);
+    }
 }
