86e904ece646cd82aa73ec138530fe968748e61f
vieira00 <matheusricardov563@gmail.com>
2024-11-22 13:24:27 -0400

    Foi criado uma classe 'MouseInputs' na onde temos um evento 'MouseEvent' que notifica quando o usuário usa o mouse.

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -1,17 +1,21 @@
 package main;
 
 import Inputs.KeyboardInputs;
+import Inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
 
+    private MouseInputs mouseInputs;
+
     public GamePanel() {
 
+        mouseInputs = new MouseInputs();
         addKeyListener(new KeyboardInputs());
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
