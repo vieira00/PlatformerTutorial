5539c8ddc46b53a9526871a8357350ade12fae84
vieira00 <matheusricardov563@gmail.com>
2024-11-28 13:25:16 -0400

    esqueci de mudar o 'initClasses()' de lugar.

--- src/main/java/main/Game.java
+++ src/main/java/main/Game.java
@@ -15,11 +15,11 @@ public class Game implements Runnable{
     private Player player;
 
     public Game() {
+        initClasses();
+
         gamePanel = new GamePanel(this);
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
-
-        initClasses();
 
         startGameLoop();
 
