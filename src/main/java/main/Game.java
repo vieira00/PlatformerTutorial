e55784950bd41b6755226839d8f930f58f958891
vieira00 <matheusricardov563@gmail.com>
2024-11-28 10:18:11 -0400

    foi feito uma mudança de renderização e um segundo teste.

--- src/main/java/main/Game.java
+++ src/main/java/main/Game.java
@@ -20,39 +20,42 @@ public class Game implements Runnable{
         gameThread.start();
     }
 
+    public void update(){
+        gamePanel.updateGame();
+    }
+
     @Override
     public void run() {
 
         double timePerFrame = 1000000000.0 / FPS_SET;
-        double timePerUpdate = 100000000.0 / UPS_SET;
-        long lastFrame = System.nanoTime();
-        long now = System.nanoTime();
+        double timePerUpdate = 1000000000.0 / UPS_SET;
 
         long previousTime = System.nanoTime();
 
         int frames = 0;
         int updates = 0;
         long lastCheck =  System.currentTimeMillis();
+
         double deltaU = 0;
+        double deltaF = 0;
 
         while(true) {
 
-            now = System.nanoTime();
             long currentTime = System.nanoTime();
 
             deltaU += (currentTime - previousTime) / timePerUpdate;
+            deltaF += (currentTime - previousTime) / timePerFrame;
             previousTime = currentTime;
 
             if(deltaU >= 1) {
-//                update();
+                update();
                 updates++;
                 deltaU--;
             }
 
-
-            if(now - lastFrame >= timePerFrame) {
+            if (deltaF >= 1) {
                 gamePanel.repaint();
-                lastFrame = now;
+                deltaF--;
                 frames++;
             }
 
@@ -61,7 +64,6 @@ public class Game implements Runnable{
                 System.out.println("FPS: " + frames + " | UPS: " + updates);
                 frames = 0;
                 updates = 0;
-
             }
         }
     }
